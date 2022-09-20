956f620920b0824b33858c3facadc48cbc52bc9e
Jonas Toth <development@jonas-toth.eu>
2022-09-20 21:29:54 +0200

    feat: implement prime-number check and sieve of eratosthenes
    - determine if a number is prime or not
    - generate prime numbers with the sieve of eratosthenes
    - add 'concepts' for natural numbers that constrain the templated
      implementations

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -11,9 +11,12 @@ set(exe_sources
 set(headers
     include/jt-computing/container/BitVector.hpp
     include/jt-computing/math/BigUInt.hpp
+    include/jt-computing/math/Concepts.hpp
+    include/jt-computing/math/NaturalNumberAlgorithms.hpp
 )
 
 set(test_sources
     lib/container/BitVector.cpp
     lib/math/BigUInt.cpp
+    lib/math/NaturalNumberAlgorithms.cpp
 )
