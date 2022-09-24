d1bce9ccde193325d89e801e254912c0ddbba57c
Jonas Toth <development@jonas-toth.eu>
2022-09-24 12:49:44 +0200

    feat: add example program to check the collatz conjecture

---
+++ bin/collatz_chain.cpp
@@ -0,0 +1,48 @@
+#include "jt-computing/math/BigUInt.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+using namespace jt;
+using namespace std;
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    assert(argc >= 1);
+    std::cerr << "Usage: " << argv[0] << "\n\n"
+              << "Determines the length of the collatz-row of <n> until '1' is "
+                 "reached.\n";
+    return EXIT_FAILURE;
+  }
+
+  assert(argc >= 2);
+  usize numberToTest{std::stoull(argv[1])};
+
+  std::cout << "Finding the length of collatz chain for '" << numberToTest
+            << "'" << std::endl;
+
+  usize chainLength{0U};
+  math::BigUInt n{numberToTest};
+  math::BigUInt peak{1U};
+
+  while (n != 1U) {
+    if (n.isEven()) {
+      // n = n / 2
+      n >>= 1U;
+    } else {
+      // n = 3n + 1
+      n *= 3U;
+      n += 1U;
+    }
+    chainLength += 1U;
+    peak = std::max(peak, n);
+  }
+
+  std::cout << "'" << numberToTest << "' took '" << chainLength
+            << "' steps to reach 1." << std::endl;
+  std::cout << "The highest number in the chain is '" << peak << "'."
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
