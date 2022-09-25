315b61e76e2c55877af9bec007553152135f1e64
Jonas Toth <development@jonas-toth.eu>
2022-09-25 15:11:26 +0200

    refactor: improve readability of example programs

--- bin/find_prime_numbers.cpp
+++ bin/find_prime_numbers.cpp
@@ -4,27 +4,25 @@
 #include <iostream>
 #include <string>
 
-using namespace jt;
 using namespace std;
+using namespace jt;
+using namespace jt::math;
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     assert(argc >= 1);
-    std::cerr << "Usage: " << argv[0] << "\n\n"
-              << "Finds the first <n> prime numbers using the sieve of "
-                 "eratosthenes. Each prime is printed on its own line\n";
+    cerr << "Usage: " << argv[0] << " <n>\n\n"
+         << "Finds the first <n> prime numbers using the sieve of "
+            "eratosthenes. Each prime is printed on its own line\n";
     return EXIT_FAILURE;
   }
 
   assert(argc >= 2);
-  usize maximumNumber{std::stoull(argv[1])};
+  usize maximumNumber{stoull(argv[1])};
 
-  std::cout << "Finding all primes up to " << maximumNumber << std::endl;
-
-  const auto primes = math::sieveEratosthenes<usize>(maximumNumber);
-
-  for (const auto prime : primes) {
-    std::cout << prime << "\n";
+  cout << "Finding all primes up to " << maximumNumber << endl;
+  for (const auto &prime : sieveEratosthenes<BigUInt>(maximumNumber)) {
+    cout << prime << "\n";
   }
 
   return EXIT_SUCCESS;
