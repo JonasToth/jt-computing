ef3f948b78a401ca2081d441b348d2f48c6fa34f
Jonas Toth <development@jonas-toth.eu>
2022-09-25 18:46:40 +0200

    fix: adjust i64 typedef to signed

--- include/jt-computing/Types.hpp
+++ include/jt-computing/Types.hpp
@@ -7,14 +7,14 @@
 namespace jt {
 using pdiff = std::ptrdiff_t;
 using usize = std::size_t;
-using i8 = std::int8_t;
-using i16 = std::int16_t;
-using i32 = std::int32_t;
-using i64 = std::uint64_t;
-using u8 = std::uint8_t;
-using u16 = std::uint16_t;
-using u32 = std::uint32_t;
-using u64 = std::uint64_t;
+using i8    = std::int8_t;
+using i16   = std::int16_t;
+using i32   = std::int32_t;
+using i64   = std::int64_t;
+using u8    = std::uint8_t;
+using u16   = std::uint16_t;
+using u32   = std::uint32_t;
+using u64   = std::uint64_t;
 } // namespace jt
 
 #endif /* end of include guard: TYPES_HPP_CITJNMAZ */
