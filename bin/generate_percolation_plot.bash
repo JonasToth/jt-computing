dab00a9ce583596c113dfb3ac259aa9d18f9573e
Jonas Toth <development@jonas-toth.eu>
2023-03-05 15:20:07 +0100

    feat: standalone example for percolation

---
+++ bin/generate_percolation_plot.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./bin/percolation_power.x 0.00 example_000.dat 2627513694
+./bin/percolation_power.x 0.05 example_005.dat 2627513694
+./bin/percolation_power.x 0.10 example_010.dat 2627513694
+./bin/percolation_power.x 0.15 example_015.dat 2627513694
+./bin/percolation_power.x 0.20 example_020.dat 2627513694
+./bin/percolation_power.x 0.25 example_025.dat 2627513694
+./bin/percolation_power.x 0.30 example_030.dat 2627513694
+./bin/percolation_power.x 0.35 example_035.dat 2627513694
+./bin/percolation_power.x 0.40 example_040.dat 2627513694
+./bin/percolation_power.x 0.45 example_045.dat 2627513694
+./bin/percolation_power.x 0.50 example_050.dat 2627513694
+./bin/percolation_power.x 0.55 example_055.dat 2627513694
+./bin/percolation_power.x 0.60 example_060.dat 2627513694
+./bin/percolation_power.x 0.65 example_065.dat 2627513694
+./bin/percolation_power.x 0.70 example_070.dat 2627513694
+./bin/percolation_power.x 0.75 example_075.dat 2627513694
+./bin/percolation_power.x 0.80 example_080.dat 2627513694
+./bin/percolation_power.x 0.85 example_085.dat 2627513694
+./bin/percolation_power.x 0.90 example_090.dat 2627513694
+./bin/percolation_power.x 0.95 example_095.dat 2627513694
+./bin/percolation_power.x 1.00 example_100.dat 2627513694
+../bin/plot_graph.gnuplot
+
