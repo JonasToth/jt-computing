c435482ab45f6bf0a41395a0dde3b5c474bff47c
Jonas Toth <development@jonas-toth.eu>
2026-04-25 17:31:41 +0200

    [feature] Fetch_Content(catch2) to build with gcc and clang

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -4,13 +4,13 @@ function(jt_executable file_name)
         PRIVATE
             ${PROJECT_NAME}::${PROJECT_NAME}
         )
-    target_compile_features(${file_name}.x PUBLIC cxx_std_23)
+    target_compile_features(${file_name}.x PUBLIC cxx_std_26)
 endfunction()
 
 function(jt_compile_setup target)
     set_target_properties(${target}
         PROPERTIES
-            CMAKE_CXX_STANDARD cxx_std_23
+            CMAKE_CXX_STANDARD cxx_std_26
             CMAKE_CXX_MODULE_STD ON
             CMAKE_CXX_EXTENSIONS OFF
             CMAKE_CXX_SCAN_FOR_MODULES ON
